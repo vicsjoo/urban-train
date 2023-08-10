8040a9d25ad7ea7c645245021a510cbaa1c1db99
João Victor Silva <87881292+vicsjoo@users.noreply.github.com>
2023-08-10 01:53:01 -0300

    still failure

--- obj/b__main.ads
+++ obj/b__main.ads
@@ -324,7 +324,7 @@ package ada_main is
    pragma Export (C, u00141, "system__concat_2B");
    u00142 : constant Version_32 := 16#2d5a86c4#;
    pragma Export (C, u00142, "system__concat_2S");
-   u00143 : constant Version_32 := 16#39ac88b5#;
+   u00143 : constant Version_32 := 16#d5bacff3#;
    pragma Export (C, u00143, "lottoB");
    u00144 : constant Version_32 := 16#1b950b9c#;
    pragma Export (C, u00144, "lottoS");
--- src/lotto.adb
+++ src/lotto.adb
@@ -4,12 +4,12 @@ package body lotto is
    function Generate_Lottery_Pick return String is
          type Int_Range is range 1..60;
          type Pick_Length is range 1..6;
-         type Lottery_Pick_Array_Type is array (Pick_Length) of Int_Range;
+         type Lottery_Pick_Array_Type is array (Pick_Length) of Integer;
          Lottery_Pick_Array : Lottery_Pick_Array_Type;
-         Random_Number : Int_Range;
+         tmpStr : String := "";
          Max_Result_Length : Integer := (2 * Integer(Pick_Length'Last)) + (Integer(Pick_Length'Last) - 1);
          Result : String (1..Max_Result_Length);  -- "01-02-03-04-05-06" + null terminator (should be 17)
-         
+         Random_Number : Int_Range;
          package Rand_Int is new Ada.Numerics.Discrete_Random(Int_Range);
          --  use Rand_Int;
          Rand_Gen : Rand_Int.Generator;
@@ -18,16 +18,35 @@ package body lotto is
          
          
       begin
---   for I in Lottery_Pick_Array'Range loop
---     loop
---        Rand_Int.Reset(Rand_Gen);
---        Random_Number := Rand_Int.Random(Rand_Gen);
---        exit when Random_Number not in Lottery_Pick_Array;
---     end loop;
---  
---     Lottery_Pick_Array (I) := Random_Number;
---  end loop;
-            
+         for Index in Lottery_Pick_Array'Range loop
+  
+               Random_Number := Rand_Int.Random(Rand_Gen);
+         
+               Lottery_Pick_Array(Index) := Integer(Random_Number);
+      
+         end loop;
+
+         -- Construct the Result string
+         Result := Integer'Image(Lottery_Pick_Array(1));
+         for Index in 2..Pick_Length'Last loop
+            Result := Result & "-" & Integer'Image(Lottery_Pick_Array(Index));
+         end loop;
+
+         return Result;
+        
+      end Generate_Lottery_Pick;
+     
+
+   package Integer_IO is new Ada.Text_IO.Integer_IO (Integer);
+   use Integer_IO;
+
+begin
+      -- Call the function to generate a random lottery pick
+      Ada.Text_IO.Put_Line(Generate_Lottery_Pick);
+     
+end Lottery;
+end lotto;
+
       --  -- Sort the numbers in ascending order
       --  
       --  -- Convert the numbers to a formatted string
@@ -36,18 +55,7 @@ package body lotto is
       --            Picks(5)'Image & "-" & Picks(6)'Image;
       --  
       --  return Result;
-         Result := "01-02-03-04-05-06";
-         --  Ada.Text_IO.Put_Line(Integer'Image(Max_Result_Length));
-         return Result;
-         
-      end Generate_Lottery_Pick;
-     
-
-   package Integer_IO is new Ada.Text_IO.Integer_IO (Integer);
-   use Integer_IO;
-
-begin
-      Ada.Text_IO.Put_Line(Generate_Lottery_Pick);
-     
-end Lottery;
-end lotto;
+      
+      --  Result := "01-02-03-04-05-06";
+      --  Ada.Text_IO.Put_Line(Integer'Image(Max_Result_Length));
+      --  return Result;
